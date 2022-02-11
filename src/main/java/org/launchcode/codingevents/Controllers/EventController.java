085d497672cd889908e97fd2159811c9aa6ad9dd
Ridgeway22 <boo22mickey@yahoo.com>
2022-02-11 16:27:21 -0600

    git commit -m "work through capter 11"

--- src/main/java/org/launchcode/codingevents/Controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/Controllers/EventController.java
@@ -4,7 +4,9 @@ package org.launchcode.codingevents.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,14 +14,14 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
-
+    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String listEvents(Model model){
-    List<String> events = new ArrayList<>();
-    events.add("Birthday");
-    events.add("Holidays");
-    events.add("Anniversary's");
+//    List<String> events = new ArrayList<>();
+//    events.add("Birthday");
+//    events.add("Holidays");
+//    events.add("Anniversary's");
     model.addAttribute("events", events);
     return "events/index";
 
@@ -29,5 +31,11 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateForm(){
         return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+    events.add(eventName);
+    return "redirect:/events";
     }
 }
